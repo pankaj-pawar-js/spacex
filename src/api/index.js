aa6648ac5225d5649a1c041ea7561fdfc477c468
Pawar <xcm7wg@splm.siemens.com>
2020-10-18 03:33:09 +0530

    set default port

--- src/api/index.js
+++ src/api/index.js
@@ -7,9 +7,10 @@ export function apiFactory(http) {
   };
 }
 
-// console.log(" process.env.PORT: ", process.env.PORT);
-
 const DEFAULT_PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
 
-const http = httpClient(`http://localhost:${DEFAULT_PORT}`);
+console.log(" process.env.PORT: ", process.env.PORT, "     HOST: ", HOST);
+
+const http = httpClient(`${HOST}:${DEFAULT_PORT}`);
 export const api = apiFactory(http);
